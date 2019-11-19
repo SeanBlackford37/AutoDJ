fa0b6ef5104e926cce60c67af3538e424ff9aa1c
Sean Blackford <sean@Seans-MacBook-Pro.local>
2019-11-19 08:25:14 -0500

    added comments

--- Song.h
+++ Song.h
@@ -4,18 +4,26 @@
 
 #ifndef HELLOWORLD_SONG_H
 #define HELLOWORLD_SONG_H
-
-//have them separating by commas
-
 #include <string>
 
 class Song {
 private:
-
+    //Artist for the song
     std::string artist;
+    //The name of the song
     std::string songTitle;
+    //Duration of the song converted into seconds from 3:20 format
     int duration;
+    //the number of times the song has been played
+    int playCount;
 
+public:
+
+
+    /**
+     * Turns the given song to a string format and is returned
+     */
+    std::string toString();
 };
 
 #endif //HELLOWORLD_SONG_H
